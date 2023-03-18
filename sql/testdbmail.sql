0c6f79c48da6f8c5e40761b943573726d49f0007
sruthyasokan <69909318+sruthyasokan@users.noreply.github.com>
2023-03-18 15:46:40 +0530

    Add files via upload

---
+++ sql/testdbmail.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: sg2nlmysql11plsk.secureserver.net:3306
+-- Generation Time: Mar 18, 2023 at 04:16 AM
+-- Server version: 5.7.26-29-log
+-- PHP Version: 7.4.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `testdbmail`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mailinfo`
+--
+
+CREATE TABLE `mailinfo` (
+  `id` int(11) NOT NULL,
+  `mailtype` varchar(20) NOT NULL,
+  `adr_sender` varchar(250) NOT NULL,
+  `adr_recipient` varchar(250) NOT NULL,
+  `subject` text NOT NULL,
+  `content` text NOT NULL,
+  `filename` varchar(250) NOT NULL,
+  `date` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mailinfo`
+--
+ALTER TABLE `mailinfo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `mailinfo`
+--
+ALTER TABLE `mailinfo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
